4f5e38b58f489f32c92885ad8c702cb026277f90
Анастасія Шевцова <nastya.sova2004@gmail.com>
2022-10-20 01:41:32 +0300

    fixed

--- lab1/DefaultAlgorithm.py
+++ lab1/DefaultAlgorithm.py
@@ -3,7 +3,7 @@ import shutil
 from functions import *
 
 
-#основний алгоритм злиття
+# основний алгоритм злиття
 def balancedMultiwayMerging(pathNotSorted: str, pathSorded: str, waysNum: str):
     pathB = []
     pathC = []
@@ -38,7 +38,7 @@ def balancedMultiwayMerging(pathNotSorted: str, pathSorded: str, waysNum: str):
             os.remove(path)
 
 
-#розділення початкового файлу
+# розділення початкового файлу
 def splitA(pathNotSorted: str, pathB: list, waysNum: str):
     fileA = FileReader(pathNotSorted)
     filesB = []
@@ -51,19 +51,19 @@ def splitA(pathNotSorted: str, pathB: list, waysNum: str):
         if fileA.current > fileA.next:
             bIndex = (bIndex + 1) % waysNum
         next(fileA)
-    
+
     fileA.close()
     for file in filesB:
         file.close()
 
 
-#злиття n файлів у інші n файлів
+# злиття n файлів у інші n файлів
 def sortMerge(pathsInputs: list, pathsOutputs: list, waysNum: int):
     readers = []
     writers = []
 
     index = 0
-    series = []
+    prev_num = None
 
     for path in pathsInputs:
         readers.append(FileReader(path))
@@ -71,36 +71,33 @@ def sortMerge(pathsInputs: list, pathsOutputs: list, waysNum: int):
     for path in pathsOutputs:
         writers.append(open(path, "wb"))
 
-    while(not isEOFReached(readers)):        
+    while (not isEOFReached(readers)):
         minNum = MAX_INT
         minIndex = -1
-        
+
         for i in range(waysNum):
             if readers[i].current:
                 number = int.from_bytes(readers[i].current, 'big')
-                if not series or number >= series[-1]:
+                if prev_num == None or number >= prev_num:
                     if number <= minNum:
                         minNum = number
                         minIndex = i
 
         if minIndex == -1:
-            for number in series:
-                writers[index].write(number.to_bytes(4, 'big'))
-            series.clear()
+            prev_num = None
             index = (index + 1) % waysNum
         else:
-            series.append(minNum)
+            writers[index].write(minNum.to_bytes(4, 'big'))
+            prev_num = minNum
             next(readers[minIndex])
 
-    for number in series:
-        writers[index].write(number.to_bytes(4, 'big'))
     for file in readers:
         file.close()
     for file in writers:
         file.close()
 
 
-#первіряє чи прочитані всі файли
+# первіряє чи прочитані всі файли
 def isEOFReached(readers):
     for file in readers:
         if file.current:
@@ -108,6 +105,6 @@ def isEOFReached(readers):
     return True
 
 
-#перевіряє чи сортування завершилося
+# перевіряє чи сортування завершилося
 def isSorted(pathA: str, pathB: str, pathC: str) -> bool:
     return os.path.getsize(pathA) == os.path.getsize(pathB) or os.path.getsize(pathA) == os.path.getsize(pathC)
